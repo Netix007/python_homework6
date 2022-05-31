7d16e64e6337c85b165e23210dede888eeef38d3
Alexandr Mandrikov <kino_mac@mail.ru>
2022-05-31 16:07:08 +0300

    Улучшена геометрия поля

--- view.py
+++ view.py
@@ -15,10 +15,10 @@ def click_button(i):
             pole[i].config(text='O')
             step[i] = 'O'
         step_x = not step_x
-
+        print(step)
 
 root = Tk()
-fontStyle = Font(family="Lucida Grande", size=100)
+fontStyle = Font(family="Lucida Grande", size=80)
 w = root.winfo_screenwidth()
 h = root.winfo_screenheight()
 button_size = int(h/6)
@@ -27,8 +27,11 @@ x = int(w/2 - window_size/2)
 y = int(h/2 - window_size/2)
 root.title('крестики-нолики 2022')
 root.geometry('{}x{}+{}+{}'.format(window_size, window_size, x, y))
-pixel = PhotoImage(width=1, height=1)
-pole = [Button(image=pixel, text=' ', font=fontStyle, height=button_size, width=button_size, compound='c',\
+for i in range(3):
+    root.columnconfigure(index=i, weight=1)
+    root.rowconfigure(index=i, weight=1)
+pixel = PhotoImage(width=button_size, height=button_size)
+pole = [Button(image=pixel, text=' ', font=fontStyle, compound='c',\
         command = (lambda z = 3*i + j: click_button(z))) for i in range(3) for j in range(3)]
 for i in range(9):
     pole[i].grid(column=i%3, row=i//3)
