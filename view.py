249cfc0bcaa2ff53b4600008e709578faf060d89
Aleksandr <kino_mac@mail.ru>
2022-06-02 14:52:08 +0300

    Исправлены ошибки

--- view.py
+++ view.py
@@ -33,36 +33,31 @@ def start():
 def click_button(i):
     global step_x, step, bot_step
     if step[i] == ' ' and not is_win(step):
-        if step_x:
-            pole[i].config(text='X')
-            step[i] = 'X'
-            if variant == 2:
-                if (is_win(step)):
-                    if step_x:
-                        result('Выиграл Игрок 2')
-                    else:
-                        result('Выиграл Игрок 1')
-                elif ' ' not in step:
-                    result('Ничья')
-                else:
-                    del bot_step[bot_step.index(i)]
-                    i = choice(bot_step)
-                    pole[i].config(text='O')
-                    step[i] = 'O'
-                    del bot_step[bot_step.index(i)]
-                    step_x = not step_x
-        else:
-            if variant == 1:
+        if variant == 1:
+            if step_x:
+                pole[i].config(text='X')
+                step[i] = 'X'
+            else:
                 pole[i].config(text='O')
                 step[i] = 'O'
-        step_x = not step_x    
-        if (is_win(step)):
-            if step_x:
-                result('Выиграл Игрок 2')
+            step_x = not step_x
+            who_win(step, step_x)
+        else:
+            pole[i].config(text='X')
+            step[i] = 'X'
+            step_x = not step_x
+            if is_win(step) or " " not in step:
+                who_win(step, step_x)
             else:
-                result('Выиграл Игрок 1')
-        elif ' ' not in step:
-            result('Ничья')
+                del bot_step[bot_step.index(i)]
+                i = choice(bot_step)
+                pole[i].config(text='O')
+                step[i] = 'O'
+                step_x = not step_x
+                if is_win(step):
+                    who_win(step, step_x)
+                else:
+                    del bot_step[bot_step.index(i)]
 
 def game_xo():
     global pole, game 
@@ -90,6 +85,8 @@ def result(a):
     result_wind = Toplevel()
     if variant == 1:
         players = 'Игрок 1 vs Игрок 2'
+    else:
+        players = 'Игрок 1 vs Бот'
     result_wind.title(f'Результаты игры: {players}')
     fontStyle = Font(family="Lucida Grande", size=30)
     w = int(result_wind.winfo_screenwidth()/2)
@@ -105,9 +102,18 @@ def result(a):
     btn2.pack(padx=20, pady=10)
     result_wind.mainloop()
 
+def who_win(step, step_x):
+    if (is_win(step)):
+        if step_x:
+            result('Выиграл Игрок 2')
+        else:
+            result('Выиграл Игрок 1')
+    elif ' ' not in step:
+        result('Ничья')
+
 def restart_game():
     global step_x, step, pole, variant
-    game.destroy()
+    #game.destroy()
     step_x = True
     step = [' ' for i in range(9)]
     pole = [9]
